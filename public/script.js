f1d38e4d78cc1bfc05766c58d8c054795f7da416
Micah Raney <micah.raney@gmail.com>
2021-11-10 12:53:29 -0800

    Added copy functionality

--- public/script.js
+++ public/script.js
@@ -3,9 +3,13 @@ $("#generate").click(function(){
 	lorem.html("");
 	var quantity = $("#quantity")[0].valueAsNumber;
 	var data = ["Lorem ipsum", "quia dolor sit", "amet", "consectetur"];
-	lorem.append("<p>")
 	for(var i = 0; i < quantity; i++){
 		lorem.append(data[i%data.length] + " ");
 	}
-	lorem.append("</p>")
+})
+
+$("#copy").click(function(){
+	var lorem = $("#lorem");
+	console.log("Copying Text");
+	navigator.clipboard.writeText(lorem.html())
 })
