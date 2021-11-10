10e292d522c378a4fe1b346ff7a4ee151fa3aaec
Micah Raney <micah.raney@gmail.com>
2021-11-10 12:46:10 -0800

    Fixed output formatting on script

--- public/script.js
+++ public/script.js
@@ -3,7 +3,9 @@ $("#generate").click(function(){
 	lorem.html("");
 	var quantity = $("#quantity")[0].valueAsNumber;
 	var data = ["Lorem ipsum", "quia dolor sit", "amet", "consectetur"];
+	lorem.append("<p>")
 	for(var i = 0; i < quantity; i++){
-		lorem.append(data[i%data.length] + "");
+		lorem.append(data[i%data.length] + " ");
 	}
+	lorem.append("</p>")
 })
