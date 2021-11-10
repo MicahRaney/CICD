6b949823b7542cf336c3621c90e54f85fd232b19
bisrade <13257311+beatrizisrade@users.noreply.github.com>
2021-11-10 13:41:39 -0800

    more realistic paragraphs + added more latin words

--- public/script.js
+++ public/script.js
@@ -2,9 +2,19 @@ $("#generate").click(function(){
 	var lorem = $("#lorem");
 	lorem.html("");
 	var quantity = $("#quantity")[0].valueAsNumber;
-	var data = ["Lorem ipsum", "quia dolor sit", "amet", "consectetur"];
+	var data = ["Lorem ipsum", "quia dolor sit", "amet", "consectetur", "Aenean sed", "tristique ante",
+	 "in consectetur tellus", "ut sed", "elit massa"];
 	for(var i = 0; i < quantity; i++){
-		lorem.append(data[i%data.length] + " ");
+		for (var j = 5; j <= Math.random(300)*300 + 5 ; j++) {
+			if (j==5) {
+				lorem.append("&emsp;&emsp;")
+			}
+			if (j%7==0){
+				lorem.append(data[j%data.length] + ". ");
+			}
+			lorem.append(data[j%data.length] + " ");
+		}
+		lorem.append("<br>")
 	}
 })
 
