cda2aed4ca36e0100c9e3016f5d763a092459999
bisrade <13257311+beatrizisrade@users.noreply.github.com>
2021-11-10 14:27:08 -0800

    Capitalization + punctuation within paragraph

--- public/script.js
+++ public/script.js
@@ -1,19 +1,28 @@
+// When "Generate" button is clicked, string of words is displayed based on # of paragraphs specified by user
 $("#generate").click(function(){
 	var lorem = $("#lorem");
 	lorem.html("");
 	var quantity = $("#quantity")[0].valueAsNumber;
 	var data = ["Lorem ipsum", "quia dolor sit", "amet", "consectetur", "Aenean sed", "tristique ante",
 	 "in consectetur tellus", "ut sed", "elit massa"];
+
+	// outer for loop controls # of paragraphs
+	// inner for loop controls # of words in paragraphs 
 	for(var i = 0; i < quantity; i++){
-		for (var j = 5; j <= Math.random(300)*300 + 5 ; j++) {
-			if (j==5) {
+		var numWords = Math.random()*50 + 20
+		for (var j = 10; j <= numWords ; j++) {
+			var randWord = Math.floor(Math.random() * 8);
+			if (j==10) {
 				lorem.append("&emsp;&emsp;")
+				lorem.append(data[randWord%data.length].charAt(0).toUpperCase() + data[randWord%data.length].slice(1));
 			}
-			if (j%7==0){
+			if (j%5==0){
 				lorem.append(data[j%data.length] + ". ");
+				lorem.append(data[j%data.length].charAt(0).toUpperCase() + data[j%data.length].slice(1));
 			}
 			lorem.append(data[j%data.length] + " ");
 		}
+		lorem.append(".")
 		lorem.append("<br>")
 	}
 })
